0d4c053c87505339e56987774ad534f5879192e6
Charles Z <zhangshiqi2003@gmail.com>
2024-05-05 18:12:23 -0400

    Update agent.py

--- train/agent/agent.py
+++ train/agent/agent.py
@@ -32,7 +32,7 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(input_size, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(128, output_size)
+        self.fc3 = nn.Linear(64, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
