01b2d34efac102664010aa5478fd408006037e26
Charles Z <zhangshiqi2003@gmail.com>
2024-05-05 18:03:51 -0400

    Added GPU support for model training

--- train/agent/agent.py
+++ train/agent/agent.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from agent.schedule import LinearSchedule
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class simpleagent(nn.Module):
     def __init__(self, input_size, output_size):
@@ -30,7 +32,7 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(input_size, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, output_size)
+        self.fc3 = nn.Linear(128, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -38,18 +40,21 @@ class DQN(nn.Module):
         x = self.fc3(x)
         return x
 
-# Define the DQN agent
-class DQNAgent:
-    def __init__(self, state_size, action_size, gamma = 0.95,epsilon = 3.0,epsilon_min=0.01):
+# Define the DQN agent and Epsilon scheduler
+scheduler = LinearSchedule(10000, 0.01, 1.0)
+
+class DQNAgent():
+    def __init__(self, state_size, action_size, memLength = 2000, gamma = 0.9,
+                 epsilon = 3.0, minimalE = 0.01, eDecay = 0.99, lr = 0.01):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = gamma   # discount rate
+        self.memory = deque(maxlen=memLength)
+        self.gamma = gamma    # discount rate
         self.epsilon = epsilon  # exploration rate
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = 0.999
-        self.model = DQN(state_size, action_size)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.1)
+        self.epsilon_min = minimalE
+        self.epsilon_decay = eDecay
+        self.model = DQN(state_size, action_size).to(device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
         self.loss_fn = nn.MSELoss()
 
     def remember(self, state, action, reward, next_state, done):
@@ -57,35 +62,38 @@ class DQNAgent:
 
     def act(self, state):
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        state = torch.FloatTensor(state).unsqueeze(0)
+            return torch.tensor(random.randrange(self.action_size), device=device)
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
         q_values = self.model(state)
         return q_values.argmax().item()
 
-    def replay(self, batch_size):
+    def replay(self, batch_size, t):
         if len(self.memory) < batch_size:
             return
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            state = torch.FloatTensor(state).unsqueeze(0)
-            next_state = torch.FloatTensor(next_state).unsqueeze(0)
+            state = torch.FloatTensor(state).unsqueeze(0).to(device)
+            next_state = torch.FloatTensor(next_state).unsqueeze(0).to(device)
             target = reward
             if not done:
                 target = reward + self.gamma * torch.max(self.model(next_state)).item()
             target_f = self.model(state).squeeze(0).tolist()
             target_f[action] = target
-            target_f = torch.FloatTensor(target_f)
+            target_f = torch.FloatTensor(target_f).to(device)
             self.optimizer.zero_grad()
             loss = self.loss_fn(self.model(state).squeeze(0), target_f)
             loss.backward()
             self.optimizer.step()
+        
         if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+            self.epsilon = scheduler.value(t)
+
 def createAgents(cfg):
     agents = []
     if(cfg["type"]=="DQN"):
         for i in range(cfg["number"]):
-            a = DQNAgent(cfg["state_size"], cfg["action_size"], cfg["memLength"], cfg["gamma"],cfg["epsilon"], cfg["minimalE"], cfg["eDecay"])
+            a = DQNAgent(cfg["state_size"], cfg["action_size"], cfg["memLength"], cfg["gamma"],cfg["epsilon"], cfg["minimalE"], cfg["eDecay"], cfg["lr"])
+            
             agents.append(a)
     #else: 
     #    for i in number:
